21bf135694a1e034b124ef23e558ad025abae586
Steffen Luypaert <steffen@linguineo.com>
2022-11-16 14:46:18 +0100

    add video plugin

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -34,6 +34,7 @@ import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformatio
 import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
 import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
 import SourceEditing from '@ckeditor/ckeditor5-source-editing/src/sourceediting';
+import Video from '@visao/ckeditor5-video/src/video';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -66,7 +67,8 @@ ClassicEditor.builtinPlugins = [
 	TableProperties,
 	TableCellProperties,
 	TextTransformation,
-	SourceEditing
+	SourceEditing,
+	Video
 ];
 
 // Editor configuration.
